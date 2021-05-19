a589d2d63bb372ad254d3f3b3c89ce3a1e469e32
Isabelle W <isabellew009@gmail.com>
2021-05-19 13:57:21 -0400

    Added MSK logo

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Homepage from './Homepage';
-import {Link, BrowserRouter as Router} from 'react-router-dom';
+import MSKlogo from '../MSKlogo.svg';
+import {Link, BrowserRouter as Router, NavLink} from 'react-router-dom';
 import './NavBar.css';
 
 
@@ -8,7 +9,15 @@ export function NavBar(props) {
     return(
         <nav>
             <div className='navbar-header'>
-                <div className='nav-button'>Logo</div>
+
+            <div className='nav-logo2'>
+                <img className='nav-logo' src= {MSKlogo} alt = 'msklogo' height = "50" />
+            </div>
+            
+            {/* <img className='nav-logo' src= {MSKlogo} alt = 'msklogo' height = "50" /> */}
+            
+                <div className='nav-button'>  
+                </div>
                 {/*
                 We will add "to="/path"" to the other buttons when we create the pages. for now they are empty
                 */}
@@ -19,6 +28,7 @@ export function NavBar(props) {
                         <Link to="/About" className="nav-button">About</Link>
                         <Link className="nav-button">Profile</Link>
                     </Router>
+                    
                  </div>
             </div>
         </nav>
