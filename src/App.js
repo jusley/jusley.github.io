28e352ca4445d760e7f0a6a952a6542678537d16
Isabelle W <isabellew009@gmail.com>
2021-05-25 16:59:28 -0400

    Created Case 1 page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { Homepage } from './components/Homepage/Homepage';
 import { About } from './components/About/About';
 import { Profile } from './components/Profile/Profile';
+import {Case} from './components/KneeModule/Case';
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 
@@ -15,6 +16,7 @@ function App() {
         <Route path='/About' exact component={About} />
         <Route path='/Profile' exact component={Profile}/>
         <Route path='/' exact component={Homepage} />
+        <Route path='/Case' exact component={Case}/>
       </Router>
     </div>
 
