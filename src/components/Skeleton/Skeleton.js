199ad52a049460cac4062583d7f3f8e4f4eb2b80
brianna-boughner <58961493+brianna-boughner@users.noreply.github.com>
2021-06-23 16:54:04 -0400

    Linked Case 1 to Sidebar

--- src/components/Skeleton/Skeleton.js
+++ src/components/Skeleton/Skeleton.js
@@ -14,7 +14,7 @@ export function Skeleton() {
                 <div className='sidebar-toggle'>
                 <a onClick={showSidebar}><h2 className="back"> &#8810;  Back</h2></a>
                     <h2 className="kneeMod">Knee Module</h2>
-                    <a href="#" className="Case1">Case 1</a>
+                    <a href="/Case" className="Case1">Case 1</a>
                     <a href="#" className="Case2">Case 2</a>
                     <a href="#" className="Case3">Case 3</a>
                     <h2 className="refMat">Reference Material</h2>
